c1dbbc2fa1c5eaa03330aeb5781771ee74bd96d3
Serhii Chernenko <serhii.chernenko@vaimo.com>
2023-09-24 11:50:39 +0300

    theme switcher added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <script setup>
+import ThemeSwitcher from './ThemeSwitcher.vue';
 </script>
 <template>
+  <ThemeSwitcher/>
+  <div class="flex justify-center items-center min-h-screen">
+    <div class="w-full max-w-[400px] p-4 bg-gray-300 dark:bg-gray-600">
+      <div class="bg-orange-400 min-h-[60px]"></div>
+    </div>
+  </div>
 </template>
---
+++ src/ThemeSwitcher.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const light = ref(null);
+
+onMounted(() => {
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+    light.value = false;
+  } else {
+    document.documentElement.classList.remove('dark');
+    localStorage.removeItem('theme');
+    light.value = true;
+  }
+});
+
+const switchTheme = () => {
+  if (light.value) {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+    light.value = false;
+
+    return false;
+  }
+
+  document.documentElement.classList.remove('dark');
+  localStorage.removeItem('theme');
+  light.value = true;
+}
+</script>
+<template>
+  <button @click="switchTheme"
+          class="absolute top-4 right-4 cursor-pointer font-bold dark:text-white"
+  >
+    {{light ? 'Dark' : 'Light'}} theme
+  </button>
+</template>
